409380143a1d8eef4d9bb8971f5b9288c66e8f64
Njaka <hei.herinjaka@gmail.com>
2026-03-26 21:35:26 +0300

    first commit

---
+++ src/main/java/com/jdbc/minidishdb/MiniDishDbApplication.java
@@ -0,0 +1,13 @@
+package com.jdbc.minidishdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDishDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDishDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jdbc/minidishdb/MiniDishDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbc.minidishdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDishDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
